83b38a8ff03ba541fc9183a5719cecffea7afe62
Kuni <guilhermekuni@gmail.com>
2021-05-22 07:57:15 -0300

    docs: add Button storires

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import Button from '.';
+import ButtonProps from './interface';
+
+export default {
+  title: 'Button',
+  component: Button,
+} as Meta;
+
+export const Primary: Story<ButtonProps> = () => (
+  <Button
+    type="submit"
+    isPrimary
+    onClick={() => alert('this is a Primary Button!')}
+  >
+    Primary
+  </Button>
+);
+
+export const Basic: Story<ButtonProps> = () => (
+  <Button type="submit" onClick={() => alert('this is a Basic Button!')}>
+    Basic
+  </Button>
+);
